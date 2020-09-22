53ae0b631a346e8e7d8f798d6cf79bbba4bce89e
Matthias Maile <matthias.maile@udo.edu>
2020-09-22 23:10:44 +0200

    alle the details added to guest class

--- guest.py
+++ guest.py
@@ -5,6 +5,12 @@ class guest:
     def __init__(self, firstname, lastname):
         self.first_name = firstname
         self.lastname = lastname
+        self.code = False
+        self.street = False
+        self.street_number = False
+        self.city = False
+        self.plz = False
+        self.phonenumber = False
 
     # setting the code the bot is going to use (each user gets his own code)
     def set_code(self, code):
@@ -14,8 +20,20 @@ class guest:
     def set_street(self, street):
         self.street = street
 
+    def set_street_number(self, street_number):
+        self.street_number = street_number
+
     def set_city(self, city):
         self.city = city
+
+    def set_plz(self, plz):
+        self.plz = plz
+
+    def set_phonenumber(self, phonenumber):
+        self.phonenumber = phonenumber
+
+    def print_adress(self):
+        print(str(self.street))
 
 
 # guest.py end 
